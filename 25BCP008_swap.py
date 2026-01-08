5a7645b2480aea2a98aa3628446d59a1f4543048
Hetvi1838 <hetvipatel1838@gmail.com>
2026-01-08 16:00:56 +0530

    25BCP008_swap.py

--- 25BCP008_swap.py
+++ 25BCP008_swap.py
@@ -1,8 +1,7 @@
 def swap():
     a=int(input("Enter a number:"))
     b=int(input("Enter a number:"))
-    a=a+b
-    b=a-b
-    a=a-b
+    b,a=a,b
     print("The swapped values are", a, b)
 swap()
+
